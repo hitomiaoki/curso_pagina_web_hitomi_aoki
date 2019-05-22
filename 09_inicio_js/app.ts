64a0edd6efbee2d299baceba855f02b19bcb5e58
Hitomi <hitomiyuki97@homail.com>
2019-05-22 23:47:28 +0200

    nuwva carpeta:datos y variable, js

--- 09_inicio_js/app.ts
+++ 09_inicio_js/app.ts
@@ -1,31 +1,32 @@
 console.log('Hola amigo')
 
+//DATOS
 
 //Tipos primitivos
 
-undefined// Tipo undefined
-20// Tipo number
-'Alejandro'// Tipo string
-true//Tipo b
+undefined // Tipo undefined
+20 // Tipo number
+'Alejandro' // Tipo string
+true //Tipo boolean
 
 
-//Tipos complejos a re
+//Tipos complejos o referenciados
 
-//nombre:'Pepe', edad:27
+//{nombre:'Pepe', edad:27} Tipo objeto
 
-//Los lenguajes
+//Los lenguajes pueden ser de este tipo:
 // Tipado fuerte  o  débil  
 // Tipado estático o dinámico
 
 
-
-// en Java seria tipado es fuerte y estático
-//Tipo 
-let b= number
+// En TypeScript el tipado  es fuerte y estático // (que no se puede cambiar)
+let b:number//Tipo number, sin valor
 b =3
-// b = 'Pepe'  Error
+// b = 'Pepe'  //Error de tipos
 
-// JS es tipado débil y dinámico
 
-let precio = 45.60 
-let producto ='Gafas'
+
+let precio = 45.60 //number
+let producto ='Gafas' //string
+
+//precio = producto //error de tipos
--- 09_inicio_js/sample.md
+++ 09_inicio_js/sample.md
@@ -7,15 +7,31 @@ Ejemplo de un párrafo en **negrita** y _cursiva_
 
 Ejemplo de un código
 
-```
+```js
 let a = 'Pepe'
 
 ```
-Ejemplo de código en línea  let b=23'
+Ejemplo de código en línea  `let b=23`
 
 
 Ejemplo de lista
 
 -Uno
+ -1.2
 -Dos
 -Tres
+
+![cherry](../6_elementos/assets/japanese_500.jpg)
+
+[Git](../6_elementos/assets/005.git)
+
+[Youtube](https://www.youtube.com)
+
+Column A | Column B | Column C 
+-------  | -------  | -------                           
+             
+ A1 | B1 | C1
+
+ A2 | B2 | C2
+
+ A3 | B3 | C3
