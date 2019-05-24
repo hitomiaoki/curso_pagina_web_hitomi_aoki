b02188aa511d2890ff327a93a3d959682c56a569
Hitomi <hitomiyuki97@homail.com>
2019-05-24 15:14:19 +0200

    nuevos añadido JS

--- 09_inicio_js/funciones.js
+++ 09_inicio_js/funciones.js
@@ -1,4 +1,3 @@
-
 'use strict'
 
 //undefined
@@ -6,28 +5,27 @@
 //number (int,float)
 //string
 //object
-//ES6
+
 
 //Declaración de la función
-function sample (){
- 
-//let saludo  = ''//
-//saludo = hola //incicializar la variable
+function sample () {
+ //ES6
+/*let saludo  = ''//Esto se llama una variable --> tipo sea undefined
+saludo = 'hola' incicializar la variable*/
 //let user= 'Pepe'//Declarar + inicializar (tipo string)
 let saludo= 'hola'
+
 //MALA PRÁCTICA
-// curso= 'JavaScript' //inicializar la variable 
+// curso= 'JavaScript' //Inicializar la variable 
 
-
-console.log(hola)   
+console.log(saludo)   
 }
 
 //let saludo = 'Hello'
 
-function saludar (nombre = 'amigo'){
- 
+function saludar (nombre = 'amigo') {
     let saludo= 'hola'
-    console.log(saludo + nombre)    
+    console.log(saludo + nombre)//Es conocido como un parámetro --> nombre ->undefined    
 }
 
 
@@ -39,119 +37,119 @@ saludar()
 
 
 
+//SEPARACIÓN DE REPONSABILIDAD  O INTERESES (separation of concerns - Edsger W. Dijkstra)
 
 
+/**
+ *JSDOc 
+ */
 
-//SEPARACION DE REPONSABILIDAD  O INTERESES ()
+ 
+/**
+ * 0/4 = 0
+ * 4/0 = Infinito
+ * 
+ * 0/0 = Indetermiación -->NaM
+ */
+
 
 /**
  *función suma
- * recibe 2 parametros
+ * @description recibe 2 parámetros y devuelve la suma de ambos
+ * recibe 2 parámetros
  * devuelve la suma de ambos
  * 
- * 
  * @param {number}
  * @param {number}
- * @return {number}
- * 
- * 
- * 
- * 
+ * @returns {number}
+ *  
+ * función pura
  */
-
-//función pura
- function suma ( a =0, b = 0){
-
+ function suma ( a =0, b = 0) {
+/*
    let c = a + b
-   return c
+   return c*/
+   return a + b
 }
 
 /**
  *función mostrar
- * Recibe un texto y lo saca por pantalla
- * 
- * @string {string}
+ * @description Recibe un texto y lo saca por pantalla
+ * @param {string / number}
  * @returns {void}
  * 
+ * función interfaz
  */
-//función interfaz
- function mostrar (mensaje) {
+ function mostrar (mensaje = '') {
   console.log(mensaje)   
 
  }
 
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 
- function dividir(a = 0, b = 1){
+   function dividir(a = 0, b = 1) {
    return  a / b
      
-
- }
+  }
 
  
+  /**
+   *@description {number} recibe 2 parámetros y devuelve la resta de ambos
+   *@param {number} a
+   *@param {number} b
+   *@returns {number}
+   */
 
- function resta(a = 0, b = 0){
-
+   function resta(a = 0, b = 0) {
    return a - b
      
   }
 
-
   /**
-   * @description {number}
    *@param {number} a
    *@param {number} b
-   *@returns {number}
-   * 
-   * 
-   * 
-   * 
+   *@returns {number} 
    */
 
   
- function multiplicar(a = 0, b = 0){
+  function multiplicar(a = 0, b = 0) {
 
     return a * b
       
    }
-  /**
-   * @description {number}
-   *@param {number} a
-   *@param {number} b
-   *@returns {number}
-   * 
-   * 
-   * 
-   * 
-   */
+  
 
-
-  function probar(){
+  function probar() {
  
-    let x = 2
-    let y = 3
+    let x = 9
+    let y = 8
 
 
- mostrar(suma(x,y))
- mostrar(suma(x))
- mostrar(suma(undefined,y))
- mostrar(suma())
- mostrar(suma())
- mostrar()
+     mostrar(suma(x,y))
+     mostrar(suma(x))
+     mostrar(suma(undefined,y))
+     mostrar(suma())
+     mostrar()
 
- mostrar(division(4,2))
- mostrar(division(4))
- mostrar(division())
- mostrar(division(undefined,2))
- mostrar()
- mostrar(resta(x,y))
- 
+     mostrar(division(4,2))
+     mostrar(division(x))
+     mostrar(division())
+     mostrar(division(undefined,2))
+     mostrar()
 
 
+     mostrar(multiplicar(x,y))
+     mostrar(multiplicar(y))
+     mostrar(multiplicar(undefined,y))
+     mostrar(multiplicar(x,'Pepe'))
+     mostrar()
 
- mostrar(division(4,2))
- mostrar(division(4))
- mostrar(division(undefined,2))
- mostrar(resta(7,3))
+     mostrar(resta(x,y))
   }
 
+   probar() 
