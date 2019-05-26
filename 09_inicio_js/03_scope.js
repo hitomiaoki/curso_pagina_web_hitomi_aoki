1d0e8eb3aa90d2c5e55f54bb7b7bd315e1338633
Hitomi <hitomiyuki97@homail.com>
2019-05-26 20:51:35 +0200

    verificacion scope

--- 09_inicio_js/03_scope.js
+++ 09_inicio_js/03_scope.js
@@ -1,12 +1,11 @@
 
-
 'user Strict'
 
-//variable de un "ámbito (scope) global"
+//variable de un *ámbito (scope) global*
 
 var antes =2
 let a = 2
-
+let b = 4
 
 function prueba (){
 //una variable un ' ámbito local' a la función
@@ -16,41 +15,45 @@ let b = 9
 
 if (true){
 
-//una variable un ' ámbito local' a la función
-    var i = 6
-// una variable de ámbito local al bloque if
-    let b = 7
+//una variable un *ámbito local* a la función
+    var z = 6
+// una variable de *ámbito local* al bloque if
+    let w = 7
 
 console.log('Es verdad') 
-console.log ('Desde el if,a') // 2 //local
-console.log ('Desde el if,b') // 9 //local
-console.log ('Desde el if,a') // 7 //local
+console.log('Desde el if',a) // 2 //local
+console.log('Desde el if',b) // 9 //local
+console.log('Desde el if',w) // 7 //local
 }  
 
 
-console.log ('Desde la función,a') // 2
-console.log ('Desde la función,b') // 9
-//console.log ('Desde el if,a') // 7
+console.log('Desde la función',a) // 2 //local
+console.log('Desde la función',b) // 9 //local
+//console.log('Desde el if',w) // 7 // local- da error porque la variable esta dentro de la función if (true)
+console.log('Desde el if',z)
 }
-
+prueba()
 
 // c = 3 daría un error debido el use strict
 
-console.log ('Desde el programa,b') // 2 //local
-console.log ('Desde el programa,a') // 4 //local
+console.log ('Desde el programa',b) // 2 //local
+console.log ('Desde el programa',a) // 4 //local d
 
 let v = 45
 
 function ejemplo (){
- let v = 3
- if (true) {
+    if (true) {
     let v = 3
     console.log(v)
  }
+  console.log(v)
+ 
 }
 
+console.log(v)
+
 function sample () {
-console.log (v)  
+//console.log (v)  
 }
 
 ejemplo()
