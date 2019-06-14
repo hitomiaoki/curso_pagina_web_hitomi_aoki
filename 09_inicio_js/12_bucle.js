6e090dce3434375b3d1110946a217e7166fb7129
hitomi <hitomiyuki97@hotmail.com>
2019-06-14 18:40:52 +0200

    bucle js

---
+++ 09_inicio_js/12_bucle.js
@@ -0,0 +1,26 @@
+
+let limite = 10
+
+let suma = 0
+for (let i = 0; i < limite; i++) {
+    let cuad = i*i
+    console.log(i, cuad)
+    suma = suma + cuad
+}
+
+console.log(suma)
+
+const aDatos = [
+    'Perro',
+    'Gatos',
+    'Caballo',
+    'Ornitorrinco'
+]
+
+for (let i = 0; i < aDatos.length; i++) {
+    let item = aDatos[i];
+    if (i > 0) {
+        item = item + 's'
+    }
+    console.log(`Tengo ${i+i} ${item}`)
+}
