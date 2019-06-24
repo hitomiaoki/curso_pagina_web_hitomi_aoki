b3388688ab1897f65784fe640033dd6893097a94
hitomi <hitomiyuki97@hotmail.com>
2019-06-24 21:27:29 +0200

    calculos

---
+++ js/spec/03_calculos.spec.js
@@ -0,0 +1,46 @@
+const f = require('../03_calculos.js')
+
+
+fdescribe('funcion de suma', () => {
+ 
+    const aErrores = [
+        "pepe", true, [], {}, NaN,null
+    ]
+
+    it('should be 5 width sumar 2 + 3', () => {
+    expect(f.sumar(2,3)).toEqual(5);
+    });
+    
+    it('should be 2 width sumar 2', () => {
+        expect(f.sumar(2)).toEqual(2);
+    });
+    
+    it('should be 0 widthout sumar arguments', () => {
+        expect(f.sumar()).toEqual(0);
+    });
+    
+    
+    it('should be 5 width sumar -2 + 7', () => {
+        expect(f.sumar(-2,7)).toEqual(5);
+    });
+
+    
+    it('should be 5.4 width sumar 2.1 + 3.3', () => {
+        expect(f.sumar(2.1,3.3)).toEqual(5.4);
+
+    });
+
+    it('should be 5 width sumar 2 + "3"', () => {
+        expect(f.sumar(2,'3')).toEqual(5);
+    });
+
+   for (let i = 0; i < aErrores.length; i++) {
+       const item = aErrores[i];
+       it('should be error width sumar 2 + no numbers', () => {
+           expect(() => f.sumar(2,item)).toThrow();
+       });
+
+   }
+
+
+});
