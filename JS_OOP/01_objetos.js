f7a3310f1b1aa56fd8d8669c784ee29d67a5f27c
hitomi <hitomiyuki97@hotmail.com>
2019-06-28 20:46:05 +0200

    objetos y datos

---
+++ JS_OOP/01_objetos.js
@@ -0,0 +1,32 @@
+const obj = new Object()
+
+const pulga = 123
+
+let garrapata
+
+const animal1 = {
+    nombre: 'Rufo',
+    edad: 3,
+    raza : 'perro',
+    parasitos: [pulga,garrapata]
+}
+
+animal2 = []
+
+const persona1 = {
+    nombre: 'Pepe',
+    edad: 23,
+    mascotas: [animal1,animal2]
+}
+
+animal1.peso = 12
+
+console.log(animal1)
+console.log(animal1.nombre)
+let propiedad = 'raza'
+console.log(animal1["raza"]) // NUNCA SE USA
+console.log(animal1[propiedad])
+
+
+
+ 
---
+++ JS_OOP/02_data.js
@@ -0,0 +1,13 @@
+const aMeses = [
+  'enero','febrero','marzo','abril','mayo','junio','julio', 
+  'agosto','septiembre','octubre','noviembre','diciembre']
+
+
+const aMesesAleman = [
+    'januar','februar','marz','april','mai','juni','juli',
+    'august','september','oktober','november','dezember']
+
+
+module.exports = {}
+module.exports.aMeses = aMeses
+module.exports.aMesesAleman = aMesesAleman
