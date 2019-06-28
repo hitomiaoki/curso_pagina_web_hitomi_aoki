8a34ccd7faca249d5ae88e196e46bbc71086d644
hitomi <hitomiyuki97@hotmail.com>
2019-06-28 20:46:23 +0200

    meses

---
+++ JS_OOP/02_meses.js
@@ -0,0 +1,48 @@
+  let(aMeses,aMesesAleman) = require('./02_data.js')
+
+
+
+function eligeArray(key) {
+    let r
+    switch (key) {
+        case 'es': 
+           r = aMeses       
+           break;
+        case 'ger':
+           r = aMesesAleman
+           break;
+        case 'en':
+           r = aMesesIngles
+           break;   
+        default:
+    }
+   
+}
+
+
+
+function aMeses() {  
+     console.log(m)
+
+
+     if (meses1.length > meses2.length) {
+         
+     } else {
+         
+     }
+
+    let aMeses = {}
+   
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+
+
+}
+
+
+
+function mostrarParesMeses() {
+  let   
+}
