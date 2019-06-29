7360f2937dabb32a621eb8331c8d012f58e390ed
Hitomi <hitomiyuki97@homail.com>
2019-06-29 18:38:03 +0200

    cambios ts app

--- 09_inicio_js/01_app.ts
+++ 09_inicio_js/01_app.ts
@@ -1,22 +1,7 @@
-console.log('Hola amigo')
 
-//DATOS
-
-//Tipos primitivos
-
-undefined // Tipo undefined
-20 // Tipo number
-'Alejandro' // Tipo string
-true //Tipo boolean
-
-
-//Tipos complejos o referenciados
-
-//{nombre:'Pepe', edad:27} Tipo objeto
-
-//Los lenguajes pueden ser de este tipo:
-// Tipado fuerte  o  débil  
-// Tipado estático o dinámico
+//Los lenguajes pueden ser
+//Tipado fuerte o débil
+//Tipado estático o dinámico
 
 
 // En TypeScript el tipado  es fuerte y estático // (que no se puede cambiar)
@@ -26,21 +11,15 @@ b =3
 // b = 'Pepe'  //Error de tipos
 
 
-
 let precio = 45.60 //number
 let producto ='Gafas' //string
 //precio = producto //error de tipos
 }
-function sumarConTipos (a:number, b: number):number{
-return a + b
+
+function suma(a:number, b: number):string{
+    let c = a + b 
+    return 'El resultado es' + c
 }
 
-/*
-function sumarConTipos (a:number, b: number):string{
-let c = a + b
-return 'El resultado es'+ c
-}*/
-
-/*suma(12,25)
-No lo aprueba porque es tipado
-suma(12,'Pepe')*/
+suma(12, 56)
+//suma(12, 'Pepe) //Daría un error de tipos
