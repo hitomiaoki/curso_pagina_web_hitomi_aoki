56d3a6c5bfee73c111e8adb068359bcbc218cc44
Hitomi <hitomiyuki97@homail.com>
2019-06-29 18:15:15 +0200

    calculos spec

--- js/spec/03_calculos.spec.js
+++ js/spec/03_calculos.spec.js
@@ -15,7 +15,7 @@ fdescribe('funcion de suma', () => {
         expect(f.sumar(2)).toEqual(2);
     });
     
-    it('should be 0 widthout sumar arguments', () => {
+    it('should be 0 with sumar widthout  arguments', () => {
         expect(f.sumar()).toEqual(0);
     });
     
@@ -36,7 +36,7 @@ fdescribe('funcion de suma', () => {
 
    for (let i = 0; i < aErrores.length; i++) {
        const item = aErrores[i];
-       it('should be error width sumar 2 + no numbers', () => {
+       it(`should be error width sumar 2 + no numbers: ${item}`, () => {
            expect(() => f.sumar(2,item)).toThrow();
        });
 
