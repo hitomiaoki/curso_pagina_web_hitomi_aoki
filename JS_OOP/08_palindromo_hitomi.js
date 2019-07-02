620b57a460079ba3843cbd5e90eeb9cff0de90a5
hitomi <hitomiyuki97@hotmail.com>
2019-07-02 21:29:09 +0200

    palindromo

---
+++ JS_OOP/08_palindromo_hitomi.js
@@ -0,0 +1,20 @@
+
+
+
+
+function isPalindromo(cadena) {
+  
+const aPalin = ['Ateo poco poeta','Ana lleva al oso la avellana']
+
+for (let i = 0; i < aPalin.length; i++) {
+    const item = aPalin[i];
+  if (cadena.split  == item.split) {
+    return true  
+  }
+  return false
+}    
+
+
+}
+
+console.log(aPalin)
