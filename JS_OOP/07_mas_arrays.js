c561277c9c486ceac79fc978e6d5c5717b15e720
hitomi <hitomiyuki97@hotmail.com>
2019-07-03 20:08:05 +0200

    mas array

--- JS_OOP/07_mas_arrays.js
+++ JS_OOP/07_mas_arrays.js
@@ -11,10 +11,54 @@ console.log(aDatos)
 aDatos.sort()
 console.log(aDatos)
 
-let aNumbers = [32, 3, 2, 21, 56, 5, 9]
-let aNumbersOriginal = aNumbers.slice()
+const aNumbers = [32, 3, 2, 21, 56, 5, 9]
+// creamos un clon de array
+const aNumbersOriginal = aNumbers.slice()
 
 aNumbers.sort((a,b) => b-a)
 console.log(aNumbers)
 console.log(aNumbersOriginal.reverse())
 console.log(aNumbersOriginal)
+
+
+let nombres = ['Luis','Rosa','Juan','Sofia','Julia','Ramon']
+
+
+// Fraccionar array : inmutable
+let seleccion = nombres.slice(1, 3)
+console.log(seleccion)
+console.log(nombres)
+
+//CLONAR
+let clon = nombres.slice()
+
+
+// Fraccionar array :  mutable
+
+let eliminados = nombres.splice(1, 3, 'pedro', 'ernesto')
+console.log(eliminados)
+console.log(nombres)
+
+
+// splice para añadir
+
+let paises = ['francia', 'italia', 'hungria', 'holanda']
+console.log(paises.splice(2,0, 'españa'))
+console.log(paises)
+
+// Pilas y calas
+
+// PILA -> stack Filo 
+//(push)la ultima posicion en quien entra) -Importante-
+//(pop)quita al final
+
+console.log(paises.push('portugal', 'alemania'))
+paises.pop()
+console.log(paises)
+
+
+// COLAS -> heap FIFO 
+//(unshift) inserta un nuevo elementos al principio
+
+paises.unshift('croacia')
+console.log
