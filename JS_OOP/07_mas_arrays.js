082a41da1005dc348e44a439e27c00fe23f62c82
Hitomi <hitomiyuki97@homail.com>
2019-07-04 22:46:46 +0200

    modificacion arrays y elementos

--- JS_OOP/07_mas_arrays.js
+++ JS_OOP/07_mas_arrays.js
@@ -15,7 +15,7 @@ const aNumbers = [32, 3, 2, 21, 56, 5, 9]
 // creamos un clon de array
 const aNumbersOriginal = aNumbers.slice()
 
-aNumbers.sort((a,b) => b-a)
+aNumbers.sort( (a,b) => a-b)
 console.log(aNumbers)
 console.log(aNumbersOriginal.reverse())
 console.log(aNumbersOriginal)
@@ -24,7 +24,7 @@ console.log(aNumbersOriginal)
 let nombres = ['Luis','Rosa','Juan','Sofia','Julia','Ramon']
 
 
-// Fraccionar array : inmutable
+// Fraccionar arrays : inmutable
 let seleccion = nombres.slice(1, 3)
 console.log(seleccion)
 console.log(nombres)
@@ -43,10 +43,10 @@ console.log(nombres)
 // splice para añadir
 
 let paises = ['francia', 'italia', 'hungria', 'holanda']
-console.log(paises.splice(2,0, 'españa'))
+console.log(paises.splice(2, 0, 'españa' ))
 console.log(paises)
 
-// Pilas y calas
+// Pilas y colas
 
 // PILA -> stack Filo 
 //(push)la ultima posicion en quien entra) -Importante-
@@ -59,6 +59,9 @@ console.log(paises)
 
 // COLAS -> heap FIFO 
 //(unshift) inserta un nuevo elementos al principio
+//(shift) elimina el primer elemento del array y devuelve dicho elemento
 
 paises.unshift('croacia')
-console.log
+console.log(paises)
+paises.shift()
+console.log(paises)
