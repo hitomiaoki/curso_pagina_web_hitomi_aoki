eb26a413049092cd3b6d8b41cba08be1637ad65b
hitomi <hitomiyuki97@hotmail.com>
2019-07-04 16:32:57 +0200

    array completo

--- JS_OOP/05_arrays.js
+++ JS_OOP/05_arrays.js
@@ -6,9 +6,10 @@ let aDatos = [2, 8, 9, 76] // new Array()
 aDatos.push(87)
 
 aDatos[100] = 9
+// console.log(aDatos[78])
 
 /** Funcion randomArray                        
- * @description  Función que devuelven un array de n nùmeros aleatorios
+ * @description:  Funcion que devuelven un array de n nùmeros aleatorios
  *               enteros y positivos menores o iguales a 100
  * @param {number} n
  * @returns {array}
@@ -32,7 +33,7 @@ console.log(String(randomArray(65)))
 /*****************************************************************/
 
 /** Funcion formatoDni
- * @description dar formato numero al dni
+ * @description dar formato número al DNI
  * @param {number | string} dni
  * @returns {string}
  * 
@@ -42,12 +43,10 @@ console.log(String(randomArray(65)))
     if(typeof dni === 'number') {
         return r
     }
-    let r = 0
     let cadenaDNI = ''
-    let aDNI = String(dni).split('')
     for (let i = 0; i < dni.length; i++) {
         const caracter = dni[i];
-            if(!isNaN(caracter)) {
+            if(caracter != ' ' && !isNaN(caracter)) {
                 cadenaDNI += caracter
             }
 
@@ -66,13 +65,8 @@ console.log(String(randomArray(65)))
 
   function letraDni(dni) {
      const aLetras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'] 
-     return  aLetras[dni%23]
+     return  aLetras[formatoDNI(dni)%23]
   }
-
- 
-
- console.log(letraDni(50993180))
- console.log(letraDni(50-99-31-80))
 
 
 
