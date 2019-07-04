640ac3a7c779d46f5ce2bf7628df1ad9531dc1fb
Hitomi <hitomiyuki97@homail.com>
2019-07-04 23:42:09 +0200

    fecha y datos cambios

---
+++ JS_OOP/12_datos.js
@@ -0,0 +1,29 @@
+
+const aMeses = [
+    'enero', 
+    'febrero',
+    'marzo',
+    'abril',
+    'mayo',
+    'junio',
+    'julio',
+    'agosto',
+    'septiembre',
+    'octubre',
+    'noviembre',
+    'diciembre']
+
+
+
+const  aDias = [
+    'domingo',
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+    'sabado']
+
+    module.exports = {}
+    module.exports.aDias = aDias
+    module.exports.aMeses = aMeses
--- JS_OOP/12_fecha_española.js
+++ JS_OOP/12_fecha_española.js
@@ -1,23 +1,20 @@
+const {aDias, aMeses} = require('./12_datos.js')
 
+ const capitalize = (cadena = ' ') => 
+       cadena[0].toUpperCase() + cadena.slice(1)
 
-/** funcion fechaEspañola
- * @description: devuelve como la  "dia de la semana" y "dia de mes del año"
+/** funcion fechaEspañola()
+ * @description: devuelve como  
+ * "dia de la semana" y "dia de mes del año"
+ * @author Hitomi Aoki
  * @param {date | string} fecha 
- * @author hitomi aoki
- * @description el string debe respetar el estandar ISO
- * @external aMeses,aDias
+ * si es string, debe respetar el estandar ISO
+ * @external aDias, aMeses
  * @returns {string}
  */
 
- const capitalize = (cadena = ' ') => cadena[0].toUpperCase() * cadena.slice()
 
-
-
-
-const aMeses = ['enero', 'febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
-const  aDias = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado']
-
-fechaEspañola = (fecha = new date() ) => {
+   const fechaEspañola = (fecha = new date() ) => {
     if (typeof fecha === 'string') {
         fecha = new Date(fecha)
     }
@@ -29,16 +26,17 @@ fechaEspañola = (fecha = new date() ) => {
 }
 
 
-
-fechaEspShort = (fecha = new date() ) => {
-    if (typeof fecha === 'string') {
-        fecha = new Date(fecha)
-    }
-    capitalize(aDias[fecha.getDay()] )
-    + fecha.getDate()
-    + capitalize(aMeses[fecha.getDay()] )
-    + fecha.getFullYear()
+const fechaEspShort = (fecha = new date() ) => {
+    fecha = (typeof fecha === 'string') ? new Date(fecha) : fecha
+    return aDias[fecha.getDay()] + ', '
+       + fecha.getDate() + ' de '
+       + aMeses [fecha.getMonth()] + ' del '
+       + fecha.getFullYear() }
 
 
-    return `${nombreDia}, ${dia} de ${mes} del ${año}`
-}
+ let f = new Date(1997,9,26)  
+ f = '1997-9-26'
+ console.log(fechaEspañola(f)) 
+ console.log(fechaEspShort(f))    
+
+
