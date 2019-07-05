45b6915169ad6ceac636b874c35b45297293d4b1
hitomi <hitomiyuki97@hotmail.com>
2019-07-05 20:28:03 +0200

    factura

---
+++ JS_OOP/15_facturas.js
@@ -0,0 +1,55 @@
+/* 
+Definir la estructura de un objeto que almacena una factura. 
+
+Las facturas están formadas por:
+- la información de la propia empresa:
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- la información del cliente
+    - nombre de la empresa
+    - dirección
+    - teléfono
+    - NIF 
+- una lista de elementos, cada uno de los cuales dispone de:
+    - descripción
+    - precio por unidad
+    - cantidad 
+- otra información básica de la factura
+    - el número de dactura
+    - tipo de IVA
+    - forma de pago
+    - la fecha
+Añadir al menos dos métodos:
+- el que calcula el importe total de la factura
+- el que muestra por pantalla la factura 
+    o al menos el importe total de la factura.
+*/
+
+
+
+          
+const factura = {
+       empresa: {
+        nombre:'San Miguel',
+       direccion: ' Manuel Avila Camacho,230',
+       telefono: '744172153',
+       NIF: '',
+},             
+Cliente: {
+    nombre:'Juan',
+    direccion: ' Agustin Lara, 69', 
+    telefono: '2817606775',
+    NIF:'',
+},
+
+items : [
+  
+
+
+
+]
+
+
+
