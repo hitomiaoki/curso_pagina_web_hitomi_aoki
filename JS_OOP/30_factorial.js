df1240b18250523471e01e8e29fd70b3f7b08c4b
hitomi <hitomiyuki97@hotmail.com>
2019-07-11 21:21:01 +0200

    factorial

---
+++ JS_OOP/30_factorial.js
@@ -0,0 +1,70 @@
+
+
+/**
+ * 1- Funcion boolean que comprueba
+ *  que un valor puede ser tratado como numero
+ * 
+ * 2-  Funcion que comprueba que es un numero valido 
+ * para poder calcular un factorial
+ *   
+ * 
+ * 3-  Funcion que calcula el factorial de un numero
+ * en caso de valores no valido, genera un error
+ * 
+ * 
+ * 4-Funcion que muestra (renderiza) los resultados por pantalla
+ *   al menos para las 2 primeras, realizar tasts unitaria
+ */
+
+
+
+
+function isValidNumber(n) {
+
+let r = false
+ if (isNaN(n) || typeof n == 'boolean'
+ || array.isArray(n)  || n === '' || n === null) {
+  r = false 
+ }
+ return r
+
+}
+
+
+
+function comprobar (i) {
+  let i = 3
+  if(('')) {
+    i = -2
+
+  } else if('') {
+    i = 0
+  } else if('') {
+    i = 1
+  } else if('') {
+    i = 2
+  }
+}
+
+function factorialCalcular (n) {
+ if (n == 0) {
+   return 1
+ }
+}
+
+
+
+
+
+
+function probar() {
+  n = 2
+  n = 3
+  n = 4 
+  n = 5
+  n = 6
+  n = 7
+  n = 8
+}
+
+probar()
