1642b6960d79b0089a93352471a378bb010ee6af
Hitomi <hitomiyuki97@homail.com>
2019-07-11 15:06:49 +0200

    relaciones cambios

--- JS_OOP/22_relaciones.js
+++ JS_OOP/22_relaciones.js
@@ -10,12 +10,12 @@ function Persona(nombre, edad, direccion) {
     this.nombre = nombre
     this.edad = edad  
     this.direccion = direccion // Asociación (atributos)
-    this.mascota = []
+    this.mascotas = []
 }
 
 Persona.prototype.comprarMascota = function(mascota) {
     
-     this.mascota.push(mascota) //Dependencia (metodo)
+     this.mascotas.push(mascota) //Dependencia (metodo)
 }
 
 function Mascota (nombre, especie) {
@@ -23,60 +23,42 @@ function Mascota (nombre, especie) {
     this.especie = especie
 }
 
-function Alumno(nombre, edad, direccion, curso) {
-      this.curso = curso
+function Alumno(nombre, edad, direccion) {
+      this.cursos = []
       Persona.prototype.constructor.call(this, nombre, edad, direccion)
-}
-
-Alumno.prototype.inscribirse = function (curso) {
-    this.cursos.push(curso)
 }
 
 Alumno.prototype = new Persona()
 Alumno.prototype.constructor = Alumno
 
+
+Alumno.prototype.inscribirse = function (curso) {
+    this.cursos.push(curso)
+}
+
+
 //-----------------------------------------------------------------------
 
-const a1 = new Alumno(
-    'Pepe', 
-     34,
-     new Direccion('c/ Pez',7,'Cádiz', 'España'));
+const a1 = new Alumno('Pepe',34,
 
-
-
+     new Direccion('c/ Pez',7,'Cádiz', 'España')) 
 console.log(a1)
 
 console.log(a1 instanceof Alumno)
 console.log(a1 instanceof Persona)
 console.log(a1 instanceof Object)
 
-a1.inscribirse('Pagina web')
+a1.inscribirse('Paginas web')
 
 const m1 = new Mascota('Amedio', 'mono')
 const m2 = new Mascota('Perry', 'ornitorrinco')
 
 
 
-p1.comprarMascota(m2) //Inyeccion de dependencia
+a1.comprarMascota(m2) //Inyeccion de dependencia
+
+console.log(a1)
 
 console.log(m2.nombre)
-console.log(p1.mascota[0].nombre)
+console.log(a1.mascotas[0].nombre)
 
-
-// distancian
-/*
-const p1 = new Persona(
-    'Pepe', 
-     34,
-     // (a:'c/ Pez',b:7, c:'Cádiz', d:'España'));
-     new Direccion('c/ Pez',7,'Cádiz', 'España'));
-
-
-const m1 = new Mascota('Amedio', 'mono')
-const m2 = new Mascota('Perry', 'ornitorrinco')
-
-p1.comprarMascota(m2) // Inyeccion de dependencia
-console.log(p1)
-
-console.log(m2.nombre)
-console.log(p1.mascota[0].nombre)*/
