26555cc68b11dac559edbb8041c869dde1cd9603
hitomi <hitomiyuki97@hotmail.com>
2019-07-15 18:50:36 +0200

    fibonacci

---
+++ JS_OOP/31_fibonacci.js
@@ -0,0 +1,44 @@
+
+/**
+ * calcula fibonacci
+ * mostrar en pantalla utilizar white de la serie Fibonacci
+ *  @param {number} limite
+ *  @returns {array}
+ * 
+ * 
+ */
+
+
+
+// const serieFibonacci = [ 0, 1, 1, 3, 5, 8]
+
+
+
+function calcularFibonacci (limite) {
+    if (limite < 1) {
+        return []
+    }
+    let i = 0
+    let j = 1
+    let aFibo = [0, 1]
+   do {
+     aFibo.push()
+       
+   } while (condition);
+  return aFibo
+}
+
+
+
+
+
+let limite = 10
+
+try {
+  console.log(calcularFibonacci(limite))  
+} catch (error) {
+    console.log(error.message)
+}
+
+
+
