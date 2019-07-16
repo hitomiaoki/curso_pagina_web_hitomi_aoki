4a08307cf18c333d12eaff600d8185015a3870ef
hitomi <hitomiyuki97@hotmail.com>
2019-07-16 21:27:40 +0200

    enigma

---
+++ JS_OOP/32_enigma.js
@@ -0,0 +1,50 @@
+/**  
+ *  1 Crear un programa con funciones de descriptación y
+ *   encriptacion
+ *  2 El algoritmo sera simplemente sustituir cada caracter por el 
+ *  coresponciente a la suma de su código UTF + el de un
+ *  número fijo usado como clave. 
+ * 
+ *  Ejemplo
+ * 
+ *  Hola <-----clave 3 -------> krncc
+ * 
+ *  @param {string}
+ *  @param  {string}
+ *  @returns
+ * 
+ */
+
+   
+
+ function descriptacion(n) {
+ 
+    if (n) {
+     
+     let = []   
+     let c = 2
+     let a = 0
+     let s = 12
+     let a = 0
+    
+     let = [ 'casa']
+
+     for (let i = 0; i <2; i++) {    
+     }
+
+    }
+
+ try {
+  console.log(descriptacion)   
+ } catch (error) {
+    console.log(error.message) 
+ }
+
+
+ }
+
+
+
+ function encriptacion () {
+
+ }
