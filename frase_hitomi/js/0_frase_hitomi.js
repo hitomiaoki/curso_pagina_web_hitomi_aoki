ee09672d1ccb3635fcdf698b3eb5252a6bfc2760
hitomi <hitomiyuki97@hotmail.com>
2019-07-19 18:50:19 +0200

    js

---
+++ frase_hitomi/js/0_frase_hitomi.js
@@ -0,0 +1,41 @@
+/**
+ * crear un programa para analizar frase:
+ *  número de palabras
+ *  longitud media de las palabras
+ *  veces cada letra
+ *  fruencia cada palabra
+ * 
+ *  @param {string}
+ * @param {string}
+ * @returns
+ */
+
+ function analizar() {
+  
+  
+
+   
+ function longitudMedia(cadena = '', lon = 0) {
+    if(cadena.length > lon) {
+       const espacios = lon - cadena.lenght    
+    } 
+    return cadena
+ }
+ 
+
+ 
+function vecesLetra(cadena, caracter) {
+   let letra = []
+   for (let i = 0; i < cadena.length; i++) {
+     if (cadena[i].toLowerCase() === caracter)letra.push(i); {
+         
+     }
+      return letra.lenght;
+   } 
+}  
+ }
+
+ 
+
+
+
