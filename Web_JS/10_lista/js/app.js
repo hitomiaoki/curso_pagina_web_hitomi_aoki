001d5d22441d469f950ec3ca56b143703474c227
hitomi <hitomiyuki97@hotmail.com>
2019-07-24 21:23:09 +0200

    lista

---
+++ Web_JS/10_lista/js/app.js
@@ -0,0 +1,60 @@
+export function app() {
+    console.log('Cargando app')  
+   
+     function prueba() {
+      class Lista extends Array {}
+     let x = new Lista()
+     console.dir(x)
+     let nodo = document.querySelector('#nodos')
+     console.dir( nodo instanceof Element)
+     console.dir( nodo instanceof HTMLElement)
+     console.dir(nodo.children)
+     console.dir(nodo.children instanceof HTMLCollection)
+     console.dir(nodo.children instanceof Array)
+     console.dir(nodo.childNodes)
+     console.dir(nodo.childNodes instanceof NodeList)
+     console.dir(nodo.childNodes instanceof Array)
+     }
+    
+    
+  {
+      
+   //Nodos 
+   let aLista = document.querySelectorAll('lista')
+   let aBoton = document.querySelector('#button')
+   
+    
+
+   // Manejadores
+   alista.forEach(enlace => enlace.addEventListener('click', onClickLista)
+     
+    );
+      
+      
+
+    // Funciones
+
+    function onClickLista(ev) {
+     
+      switch (ev.target) {
+        case value:
+          
+          break;
+      
+        case value:
+          break;
+      }
+    }
+
+
+    }
+
+
+
+
+
+
+
+ }
+
+ 
