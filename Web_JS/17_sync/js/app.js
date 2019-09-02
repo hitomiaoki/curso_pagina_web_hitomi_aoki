3855d994a904e08072abc14592237f104fa0dc9f
hitomi <hitomiyuki97@hotmail.com>
2019-09-02 18:40:36 +0200

    sync

---
+++ Web_JS/17_sync/js/app.js
@@ -0,0 +1,50 @@
+export function app() {
+    console.log('Cargando app')
+
+    const store = 'userName'
+
+    // NODOS DEL DOM
+     let btnGuardar = document.querySelector('#btn-guardar')
+     let btnBorrar = document.querySelector('#btn-borrar')
+     let inNombre = document.querySelector('#in-nombre')
+     let spanSaludo = document.querySelector('#span-saludo')
+
+
+    // Asociación de manejadores de eventos
+    btnGuardar.addEventListener('click', onClickGuardar)
+    btnBorrar.addEventListener('click', onClickBorrar)
+    inNombre.addEventListener('input', onInputNombre )
+    //inNombre.addEventListener('change', onChangeNombre)
+
+
+    // Inicialización de valores
+    let user = localStorage.getItem(store)
+    if (user) {
+        inNombre.value = user
+        spanSaludo.innerHTML = user 
+    } 
+
+
+    function onClickGuardar() {
+        localStorage.setItem(store, inNombre.value)
+    }
+
+
+  // Funciones manejadores de evento
+    function onClickBorrar(ev) {
+        inNombre.value = ''
+        spanSaludo.innerHTML =  inNombre.value 
+        localStorage.removeItem(store) 
+    }
+
+    function onInputNombre(ev) {
+        spanSaludo.innerHTML = ev.target.value
+    }
+
+    /*function onChangeNombre(ev) {
+        console.log('change', ev.target.value)
+    }*/
+
+
+
+}
